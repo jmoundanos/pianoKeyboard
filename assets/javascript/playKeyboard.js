db8641820372ad64d1fbe1cfc057a34036331ebe
Jennifer Moundanos <jmoundanos@gmail.com>
2020-10-05 20:42:40 -0500

    add html changes

--- assets/javascript/playKeyboard.js
+++ assets/javascript/playKeyboard.js
@@ -1,209 +1,164 @@
 function playKeyboard(){
 
-	let pressColor = '#008000'; //color when key is pressed
+	let pressColor = '#1BC0EA'; //color when key is pressed
 
 
-	// var isMobile = !!navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
-	// if(isMobile) { var evtListener = ['touchstart', 'touchend']; } else { var evtListener = ['mousedown', 'mouseup']; }
+	var isMobile = !!navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
+	if(isMobile) { var evtListener = ['touchstart', 'touchend']; } else { var evtListener = ['mousedown', 'mouseup']; }
 
 	var __audioSynth = new AudioSynth();
 	__audioSynth.setVolume(0.5);
-	var __octave = 4; //set position of middle C, which is normally the 4th octave
+	var __octave = 4; //sets position of middle C, normally the 4th octave
 	
 
 	// Key bindings, notes to keyCodes.
 	var keyboard = {
 			
-			// /* ~ */
-			// 192: 'C,-2',
+			/* ~ */
+			192: 'C,-2',
 
-			// /* 1 */
-			// 49: 'C#,-2',
+			/* 1 */
+			49: 'C#,-2',
 
-			// /* 2 */
-			// 50: 'D,-2',
+			/* 2 */
+			50: 'D,-2',
 
-			// /* 3 */
-			// 51: 'D#,-2',
+			/* 3 */
+			51: 'D#,-2',
 
-			// /* 4 */
-			// 52: 'E,-2',
+			/* 4 */
+			52: 'E,-2',
 
-			// /* 5 */
-			// 53: 'F,-2',
+			/* 5 */
+			53: 'F,-2',
 
-			// /* 6 */
-			// 54: 'F#,-2',
+			/* 6 */
+			54: 'F#,-2',
 
-			// /* 7 */
-			// 55: 'G,-2',
+			/* 7 */
+			55: 'G,-2',
 
-			// /* 8 */
-			// 56: 'G#,-2',
+			/* 8 */
+			56: 'G#,-2',
 
-			// /* 9 */
-			// 57: 'A,-2',
+			/* 9 */
+			57: 'A,-2',
 
-			// /* 0 */
-			// 48: 'A#,-2',
+			/* 0 */
+			48: 'A#,-2',
 
-			// /* - */
-			// 189: 'B,-2',
+			/* - */
+			189: 'B,-2',
 
-			// /* = */
-			// 187: 'C,-1',
+			/* = */
+			187: 'C,-1',
 
-			// /* Q */
-			// 81: 'C#,-1',
+			/* Q */
+			81: 'C#,-1',
 
-			// /* W */
-			// 87: 'D,-1',
+			/* W */
+			87: 'D,-1',
 
-			// /* E */
-			// 69: 'D#,-1',
+			/* E */
+			69: 'D#,-1',
 
-			// /* R */
-			// 82: 'E,-1',
+			/* R */
+			82: 'E,-1',
 
-			// /* T */
-			// 84: 'F,-1',
+			/* T */
+			84: 'F,-1',
 
-			// /* Y */
-			// 89: 'F#,-1',
+			/* Y */
+			89: 'F#,-1',
 
-			// /* U */
-			// 85: 'G,-1',
+			/* U */
+			85: 'G,-1',
 
-			// /* I */
-			// 73: 'G#,-1',
+			/* I */
+			73: 'G#,-1',
 
-			// /* O */
-			// 79: 'A,-1',
+			/* O */
+			79: 'A,-1',
 
-			// /* P */
-			// 80: 'A#,-1',
+			/* P */
+			80: 'A#,-1',
 
-			// /* [ */
-			// 219: 'B,-1',
+			/* [ */
+			219: 'B,-1',
 
-			// /* ] */
-			// 221: 'C,0',
+			/* ] */
+			221: 'C,0',
 
-			// /* A */
-			// 65: 'C#,0',
+			/* A */
+			65: 'C#,0',
 
-			// /* S */
-			// 83: 'D,0',
+			/* S */
+			83: 'D,0',
 
-			// /* D */
-			// 68: 'D#,0',
+			/* D */
+			68: 'D#,0',
 
-			// /* F */
-			// 70: 'E,0',
+			/* F */
+			70: 'E,0',
 
-			// /* G */
-			// 71: 'F,0',
+			/* G */
+			71: 'F,0',
 
-			// /* H */
-			// 72: 'F#,0',
+			/* H */
+			72: 'F#,0',
 
-			// /* J */
-			// 74: 'G,0',
+			/* J */
+			74: 'G,0',
 
-			// /* K */
-			// 75: 'G#,0',
+			/* K */
+			75: 'G#,0',
 
-			// /* L */
-			// 76: 'A,0',
+			/* L */
+			76: 'A,0',
 
-			// /* ; */
-			// 186: 'A#,0',
+			/* ; */
+			186: 'A#,0',
 
-			// /* " */
-			// 222: 'B,0',
+			/* " */
+			222: 'B,0',
 			
 
-			// /* Z */
-			// 90: 'C,1',
+			/* Z */
+			90: 'C,1',
 
-			// /* X */
-			// 88: 'C#,1',
+			/* X */
+			88: 'C#,1',
 
-			// /* C */
-			// 67: 'D,1',
+			/* C */
+			67: 'D,1',
 
-			// /* V */
-			// 86: 'D#,1',
+			/* V */
+			86: 'D#,1',
 
-			// /* B */
-			// 66: 'E,1',
+			/* B */
+			66: 'E,1',
 
-			// /* N */
-			// 78: 'F,1',
+			/* N */
+			78: 'F,1',
 
-			// /* M */
-			// 77: 'F#,1',
+			/* M */
+			77: 'F#,1',
 
-			// /* , */
-			// 188: 'G,1',
+			/* , */
+			188: 'G,1',
 
-			// /* . */
-			// 190: 'G#,1',
+			/* . */
+			190: 'G#,1',
 
-			// /* / */
-			// 191: 'A,1',
+			/* / */
+			191: 'A,1',
 
-			// /* <- */
-			// 37: 'A#,1',
+			/* <- */
+			37: 'A#,1',
 
-			// /* -> */
-			// 39: 'B,1',
-        
-            //b
-            66: 'C, 0',
-
-            //v
-            86: 'B, 0',
-            
-            //c
-            67: 'A, 0',
-
-            //x
-            88: 'G, -1',
-
-            //d
-            68: 'G#, -1',
-
-            //f
-            70: 'A#, 0',
-
-            //z
-            90: 'E, -1',
-            
-            //h
-            72: 'C#, 0',
-
-            //n
-            78: 'D, 0',
-
-            //j
-            74: 'D#, 0',
-
-            //m
-            77: 'E, 0',
-
-            //,
-            188: 'F, 0',
-
-            //l
-            76: 'F#, 0',
-
-            //.
-            190: 'G, 0',
-
-            ///
-            191: 'G#, 0'
-
+			/* -> */
+			39: 'B,1',
+		
 		};
 	
 	var reverseLookupText = {};
@@ -216,22 +171,22 @@ function playKeyboard(){
 
 		switch(i|0) { //some characters don't display like they are supposed to, so need correct values
 		
-			// case 187: //equal sign
-			// 	val = 61; //???
-			// 	break;
+			case 187: //equal sign
+				val = 61; //???
+				break;
 			
-			// case 219: //open bracket
-			// 	val = 91; //left window key
-			// 	break;
+			case 219: //open bracket
+				val = 91; //left window key
+				break;
 			
-			// case 221: //close bracket
-			// 	val = 93; //select key
-			// 	break;
+			case 221: //close bracket
+				val = 93; //select key
+				break;
 			
 			case 188://comma
 				val = 44; //print screen
 				break;
-			
+			//the fraction 3/4 is displayed for some reason if 190 wasn't replaced by 46; it's still the period key either way
 			case 190: //period
 				val = 46; //delete
 				break;
